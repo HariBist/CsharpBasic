dab76443f26526498e5bb4b477a5ebe0a0cadd56
HariBist <haribist1001@gmail.com>
2024-04-18 03:28:47 +0545

    first commit

---
+++ BasicLinq/Program.cs
@@ -0,0 +1,14 @@
+﻿// int[] age = {12,34,24,22,4,45,32,33,22,12,3,2};
+// var a = from i in age where i<20  orderby i select i;
+// foreach(int item in a)
+// {
+//     Console.WriteLine(item);
+// }
+// Console.ReadLine();
+
+string[] names ={ "hari", "radha","krishna"};
+var b = from name in names where name.Contains("i") select name;
+foreach(string item in b)
+{
+    Console.WriteLine(item);
+}
