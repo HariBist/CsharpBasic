abc77b6716dba07023906f37f9bf4a563bbd864c
HariBist <haribist1001@gmail.com>
2024-04-18 03:51:11 +0545

    foreach loop used

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -31,29 +31,43 @@
 // {
 //     Console.WriteLine("What does the fox say?");
 // }
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage =0;
-Console.WriteLine(daysUntilExpiration);
-if(daysUntilExpiration==0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-else if(daysUntilExpiration==1){
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage=20;
-}
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage =0;
+// Console.WriteLine(daysUntilExpiration);
+// if(daysUntilExpiration==0)
+// {
+//     Console.WriteLine("Your subscription has expired.");
+// }
+// else if(daysUntilExpiration==1){
+//     Console.WriteLine("Your subscription expires within a day!");
+//     discountPercentage=20;
+// }
 
-    if(daysUntilExpiration<=5)
+//     if(daysUntilExpiration<=5)
+//     {
+//         Console.WriteLine($"Your suscritption expires in {daysUntilExpiration} days");
+//         discountPercentage =10;
+//     }
+//     else if(daysUntilExpiration<=10)
+// {
+//     Console.WriteLine("Your subscription will expire soon. Renew now!");
+// }
+// if(discountPercentage>0)
+// {
+//     Console.WriteLine($"Renew now and save {discountPercentage}%");
+// }
+
+// int[] num = new int[3];
+// num[0]=3;
+// num[2]=4;
+// num[1]=5;
+string[] names = {"asmita","susmita","dipa","lokendra","hari"};
+
+foreach(string name in names)
+{
+    if(name.Contains("i"))
     {
-        Console.WriteLine($"Your suscritption expires in {daysUntilExpiration} days");
-        discountPercentage =10;
+        Console.WriteLine($"name:{name}");
     }
-    else if(daysUntilExpiration<=10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-if(discountPercentage>0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%");
 }
